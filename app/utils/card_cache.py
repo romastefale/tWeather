d86aff43b706307b0dcc25002138f1cabc5e9c47
@tigrao <romastefale@gmail.com>
2026-05-08 05:02:59 -0300

    feat: add ttl to card cache

--- app/utils/card_cache.py
+++ app/utils/card_cache.py
@@ -1,11 +1,42 @@
+import time
+
 CARD_CACHE = {}
+TTL_SECONDS = 600
 
 
 
 def get_card_cache(key: str):
-    return CARD_CACHE.get(key)
+    item = CARD_CACHE.get(key)
+
+    if not item:
+        return None
+
+    created_at = item['created_at']
+
+    if time.time() - created_at > TTL_SECONDS:
+        CARD_CACHE.pop(key, None)
+        return None
+
+    return item['data']
 
 
 
 def save_card_cache(key: str, data):
-    CARD_CACHE[key] = data
+    CARD_CACHE[key] = {
+        'created_at': time.time(),
+        'data': data,
+    }
+
+
+
+def cleanup_card_cache():
+    now = time.time()
+
+    expired = []
+
+    for key, item in CARD_CACHE.items():
+        if now - item['created_at'] > TTL_SECONDS:
+            expired.append(key)
+
+    for key in expired:
+        CARD_CACHE.pop(key, None)
