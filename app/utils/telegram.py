c7f0f30b16840b11a03d1f092eff278e33d34bef
@tigrao <romastefale@gmail.com>
2026-05-08 08:20:39 -0300

    improve: harden safe_edit_text against telegram edge cases

--- app/utils/telegram.py
+++ app/utils/telegram.py
@@ -5,6 +5,8 @@ from aiogram.types import CallbackQuery
 
 logger = logging.getLogger(__name__)
 
+MAX_TEXT_LENGTH = 4000
+
 
 async def safe_edit_text(
     callback: CallbackQuery,
@@ -12,12 +14,15 @@ async def safe_edit_text(
     reply_markup=None,
 ):
     try:
+        text = text[:MAX_TEXT_LENGTH]
+
         if callback.inline_message_id:
             await callback.bot.edit_message_text(
                 inline_message_id=callback.inline_message_id,
                 text=text,
                 reply_markup=reply_markup,
                 parse_mode="HTML",
+                disable_web_page_preview=True,
             )
             return
 
@@ -33,19 +38,27 @@ async def safe_edit_text(
             text,
             reply_markup=reply_markup,
             parse_mode="HTML",
+            disable_web_page_preview=True,
         )
 
     except TelegramBadRequest as error:
         error_text = str(error).lower()
 
-        if "message is not modified" in error_text:
-            return
+        ignored_errors = [
+            "message is not modified",
+            "message to edit not found",
+            "query is too old",
+            "message can't be edited",
+            "there is no text in the message to edit",
+        ]
 
-        if "message to edit not found" in error_text:
-            return
-
-        if "query is too old" in error_text:
-            return
+        for ignored_error in ignored_errors:
+            if ignored_error in error_text:
+                logger.warning(
+                    "Ignored Telegram edit issue: %s",
+                    ignored_error,
+                )
+                return
 
         logger.exception("Telegram edit failed")
 
